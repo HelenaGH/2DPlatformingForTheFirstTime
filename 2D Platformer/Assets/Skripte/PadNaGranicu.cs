19f06447ded28c1a5d9cfb54e0d5310ce630b4fc
HelenaGH <47184541+HelenaGH@users.noreply.github.com>
2020-04-18 11:55:05 +0200

    WorkInProgressII

---
+++ 2D Platformer/Assets/Skripte/PadNaGranicu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PadNaGranicu : MonoBehaviour 
+{
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (other.gameObject.CompareTag("Player"))
+		{
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
